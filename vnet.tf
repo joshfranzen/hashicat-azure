6dfcb373c681510c4d330cbab6b6634f8b0f2708
Josh Franzen <jfranzen8705@gmail.com>
2021-09-16 20:03:28 +0000

    added vnet module

---
+++ vnet.tf
@@ -0,0 +1,6 @@
+module "vnet" {
+  source  = "app.terraform.io/jfranzen-training/vnet/azurerm"
+  version = "2.5.0"
+  # insert required variables here
+  resource_group_name = azurerm_resource_group.myresourcegroup.name
+}
