479dbdd1a0f7d7b4f2dd2019e4789bd2569cc138
Josh Franzen <jfranzen8705@gmail.com>
2021-09-16 20:26:22 +0000

    update vnet module

--- vnet.tf
+++ vnet.tf
@@ -1,6 +1,6 @@
-module "vnet" {
-  source  = "app.terraform.io/jfranzen-training/vnet/azurerm"
-  version = "2.5.1"
+module "network" {
+  source  = "app.terraform.io/jfranzen-training/network/azurerm"
+  version = "3.5.0"
   # insert required variables here
   resource_group_name = azurerm_resource_group.myresourcegroup.name
 }
